fb71e3b3058165769cb0ed21b08e0a197c24b6c4
Steven Arthur <steven.arthur.degen@gmail.com>
2024-01-29 16:16:12 -0500

    Updated script.js
    Added comments to my work and deleted isP variable

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+//Declaring all the elements from the HTML file
 const button = document.getElementById('check-btn');
 const input = document.getElementById('text-input');
 const result = document.getElementById('result');
-const isP = true
 
+//This function will check if input is a palindrome
 const check = () => {
+
+    //This regular expresion will check for any numbers or non letter characters 
     const regex = /[\W - \d]/g;
+    //Formatting base input in to an array changing letters to lower case and running my regex
     const wordOrPhrase = input.value.toLowerCase().replace(regex, '').split('');
+    //Reversing user input and formatting 
     const reverse = input.value.toLowerCase().replace(regex, '').split('').reverse();
+    //Original user input
     const joined = input.value;
     
+    //this will loop through wordOrPhrase and check if reverse matches up if not will return not palindrome
+    //Otherwise function will return is palindrome
     for(let i = 0; i < wordOrPhrase.length; i++){
         if(reverse[i] !== wordOrPhrase[i]){
             return result.innerHTML = `
@@ -24,4 +32,5 @@ const check = () => {
 
 }
 
+//Added functionality to button
 button.addEventListener('click', check);
